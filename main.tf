6ef72546e5b8c7a5ed5a0b746a67af6ed0d16982
Richard Downe <richard@casetext.com>
2015-10-26 11:27:01 -0700

    Update main.tf
    variable all_amis must be declared in order for the results of the lookup to be stored there.

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 variable "region" {}
 variable "channel" {}
 variable "virttype" {}
+variable "all_amis" {}
 
 output "ami_id" {
     value = "${lookup(var.all_amis, format(\"%s-%s-%s\", var.channel, var.region, var.virttype))}"
