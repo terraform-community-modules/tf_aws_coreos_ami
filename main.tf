ac2b036d2435e325a92f9739e27692aaa59c6175
Anton Babenko <anton@antonbabenko.com>
2016-10-27 12:40:29 +0200

    Added compiled variables.tf.json (fix #7)

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ variable "region" {}
 variable "channel" {}
 variable "virttype" {}
 
+variable "all_amis" {
+    default = ""
+}
+
 output "ami_id" {
     value = "${lookup(var.all_amis, format("%s-%s-%s", var.channel, var.region, var.virttype), "")}"
 }
