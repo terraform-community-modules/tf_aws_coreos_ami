f9aa5ae7c36b2cc1505be1a43abe5463ae3b893c
Paul Tyng <ptyng@underarmour.com>
2016-10-31 16:09:40 -0400

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 variable "region" {}
+
 variable "channel" {}
+
 variable "virttype" {}
 
-
 output "ami_id" {
-    value = "${lookup(var.all_amis, format("%s-%s-%s", var.channel, var.region, var.virttype), "")}"
+  value = "${lookup(var.all_amis, format("%s-%s-%s", var.channel, var.region, var.virttype), "")}"
 }
-
