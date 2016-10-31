96602703f6c5181ed9e872fcdf4f2978b1088e49
girish ramnani <girishramnani95@gmail.com>
2016-10-31 22:17:12 +0530

    removing the extra all_ami variable from main.tf file

--- main.tf
+++ main.tf
@@ -2,9 +2,6 @@ variable "region" {}
 variable "channel" {}
 variable "virttype" {}
 
-variable "all_amis" {
-    default = ""
-}
 
 output "ami_id" {
     value = "${lookup(var.all_amis, format("%s-%s-%s", var.channel, var.region, var.virttype), "")}"
